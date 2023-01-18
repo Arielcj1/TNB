93ab9d11418c0634e3555becfb888ce73d462055
ArielCJ <ariel.callejas@believesol.com>
2023-01-18 16:35:18 -0400

    elemento

--- cypress/TheNewBlock/Login.js
+++ cypress/TheNewBlock/Login.js
@@ -6,18 +6,17 @@ export class Login {
   type_password(pass) {
     cy.get(".felx-col > :nth-child(2) > div.w-full > .w-full").type(pass);
   }
-  clic_sign_in(){
-    cy.get('.bg-gunmetalLight').click()
-    cy.wait(1000)
+  clic_sign_in() {
+    cy.get(".bg-gunmetalLight").click();
+    cy.wait(1000);
   }
-  clic_AskForCode(){
-    cy.get('.flex > [type="button"]').click()
+  clic_AskForCode() {
+    cy.get('.justify-around > [type="button"]').click();
   }
-  type_Code(code){
-    cy.get('div.w-full > .w-full').type(code)
+  type_Code(code) {
+    cy.get("div.w-full > .w-full").type(code);
   }
-  clic_Enter(){
-    cy.get('[type="submit"]').click()
+  clic_Enter() {
+    cy.get('[type="submit"]').click();
   }
-  
 }
